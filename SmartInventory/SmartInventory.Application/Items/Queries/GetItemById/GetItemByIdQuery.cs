024ea76c6fd15f077039e25496ed6e5c237617fa
KimuthuKisal <kimuthukisal23@gmail.com>
2025-12-26 14:21:23 +0530

    Get Item by Id

---
+++ SmartInventory/SmartInventory.Application/Items/Queries/GetItemById/GetItemByIdQuery.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using SmartInventory.Application.Items.Queries.GetItems;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartInventory.Application.Items.Queries.GetItemById
+{
+    public class GetItemByIdQuery : IRequest<ItemViewModel>
+    {
+        public int ItemId { get; set; }
+    }
+}
---
+++ SmartInventory/SmartInventory.Application/Items/Queries/GetItemById/GetItemByIdQueryHandler.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using MediatR;
+using SmartInventory.Application.Items.Queries.GetItems;
+using SmartInventory.Domain.Entity;
+using SmartInventory.Domain.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartInventory.Application.Items.Queries.GetItemById
+{
+    public class GetItemByIdQueryHandler : IRequestHandler<GetItemByIdQuery, ItemViewModel>
+    {
+        private readonly IItemRepository _itemRepository;
+        private readonly IMapper _mapper;
+
+        public GetItemByIdQueryHandler(IItemRepository itemRepository, IMapper mapper)
+        {
+            _itemRepository = itemRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<ItemViewModel> Handle(GetItemByIdQuery request, CancellationToken cancellationToken)
+        {
+            Item item = await _itemRepository.GetItemById(request.ItemId);
+            ItemViewModel itemView = _mapper.Map<ItemViewModel>(item);
+            return itemView;
+        }
+    }
+}
